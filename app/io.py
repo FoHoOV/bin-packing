a756217f2c1735aa5f72efcce5971d33c82bf59b
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-12-12 00:20:51 +0330

    write beautified output

--- app/io.py
+++ app/io.py
@@ -1,16 +1,21 @@
 import csv
 import os
 import sys
-from typing import List
+from io import StringIO
 
+import pandas
 from pydantic import ValidationError
 
 from models.binpacking import Item
 
 
-def read_items_from_csv(filepath: str) -> List[Item]:
+def read_items_from_csv(filepath: str) -> list[Item]:
     items = []
     print("reading file")
+
+    if not filepath.endswith(".csv"):
+        raise ValidationError("only csv files are allowed")
+
     with open(filepath, "r", newline="", encoding="utf-8") as f:
         reader = csv.DictReader(f)
         for row in reader:
@@ -23,17 +28,41 @@ def read_items_from_csv(filepath: str) -> List[Item]:
     return items
 
 
-def write_output(filepath: str, bins: List[List[Item]], capacity: float) -> None:
+def _write_csv(filepath: str, buffer: StringIO):
+    with open(filepath, "w", newline="", encoding="utf-8") as f:
+        f.writelines(buffer.readlines())
+
+
+def _write_table(filepath: str, buffer: StringIO):
+    pandas.read_csv(buffer).to_markdown(
+        os.path.join(
+            os.path.dirname(filepath), f"{os.path.basename(filepath)}-pretty.csv"
+        ),
+        index=False,
+    )
+
+
+def write_output(filepath: str, bins: list[list[Item]], capacity: float) -> None:
     print("writing result")
+
+    if not filepath.endswith(".csv"):
+        raise ValidationError("only csv files are allowed")
+
     # Ensure output directory exists
     output_dir = os.path.dirname(filepath)
     if output_dir and not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
 
-    with open(filepath, "w", newline="", encoding="utf-8") as f:
-        writer = csv.writer(f)
-        writer.writerow(["container_id", "name", "weight", "sum"])
-        for b_idx, bin_items in enumerate(bins):
-            total = sum(it.weight for it in bin_items)
-            for it in bin_items:
-                writer.writerow([b_idx, it.name, it.weight, total])
+    buffer = StringIO()
+    writer = csv.writer(buffer)
+    writer.writerow(["container_id", "name", "weight", "sum"])
+    for bin_index, bin_items in enumerate(bins):
+        total = sum(item.weight for item in bin_items)
+        for item in bin_items:
+            writer.writerow([bin_index + 1, item.name, item.weight, total])
+
+    buffer.seek(0)
+    _write_csv(filepath, buffer)
+
+    buffer.seek(0)
+    _write_table(filepath, buffer)
