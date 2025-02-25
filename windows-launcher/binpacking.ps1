2a75a96061d4107cd3c40be18422f1fd13d732b3
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2025-02-25 12:08:27 +0330

    another try at correctly running the main file with pipenv

--- windows-launcher/binpacking.ps1
+++ windows-launcher/binpacking.ps1
@@ -93,8 +93,7 @@ Write-Output "Added CBC bin folder ($cbcBinPath) to PATH for this session."
 Push-Location $binPackingDir
 python -m pip install pipenv --user
 python -m pipenv install
-python -m pipenv shell 
-python -m main $arguments
+python -m pipenv python main $arguments
 Pop-Location
 
 Read-Host -Prompt "Press Enter to exit"
